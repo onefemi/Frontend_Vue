d177061ed71e2160c0ebe30380f59231940e8b2e
onefemi <46090150+onefemi@users.noreply.github.com>
2018-12-31 14:47:33 +0100

    User Service
    Service to add new users

--- src/services/serviceRegister.js
+++ src/services/serviceRegister.js
@@ -3,5 +3,8 @@ import api from "@/services/api";
 export default {
   register(credentials) {
     return api().post("register", credentials);
+  },
+  adduser(credentials) {
+    return api().post("adduser", credentials);
   }
 };
